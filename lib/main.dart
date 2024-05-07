6cbb47763bcc9ce93bbca685d119349319bac79c
Fady <fady33zaher@gmail.com>
2024-05-07 08:18:26 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flavor/flavors.dart';
-import 'package:flavor/home_screen.dart';
+import 'package:flavor/src/presentation/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
